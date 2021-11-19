c76fb944f4800a740574a0bfdd8a6c600810b228
Juanes <juanes303@gmail.com>
2021-11-18 22:48:20 -0500

    se agrega el proyecto

---
+++ inventario/Controllers/inventarioController.cs
@@ -0,0 +1,70 @@
+﻿using inventario.Dato;
+using inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace inventario.Controllers
+{
+    public class inventarioController : Controller
+    {
+
+        InventarioAdmin admin = new InventarioAdmin();
+        // GET: inventario
+        public ActionResult Index()
+        {
+            IEnumerable<registros> lista = admin.Consultar();
+            ViewBag.mensaje = "";
+            return View(lista);
+        }
+
+        //vista de guardado
+        public ActionResult Guardar()
+        {
+            ViewBag.mensaje = "";
+            return View();
+        }
+
+        public ActionResult Nuevo(registros modelo)
+        {
+            admin.Guardar(modelo);
+            ViewBag.mensaje = "Registro guardado";
+            return View("Guardar", modelo);
+        }
+
+        public ActionResult Detalle(int ID = 0)
+        {
+            registros modelo = admin.Consultar(ID);
+            return View(modelo);
+        }
+
+        public ActionResult Modificar(int ID)
+        {
+            registros modelo = admin.Consultar(ID);
+            ViewBag.mensaje = "";
+            return View(modelo);
+        }
+
+        public ActionResult Actualizar(registros modelo)
+        {
+            admin.Modificar(modelo);
+            ViewBag.mensaje = "Registro Modificado";
+            return View("Modificar", modelo);
+        }
+
+        public ActionResult Eliminar(int ID = 0)
+        {
+            registros modelo = new registros()
+            {
+                ID = ID
+            };
+            admin.Eliminar(modelo);
+            IEnumerable<registros> lista = admin.Consultar();
+            ViewBag.mensaje = "Registro Eliminado";
+            return View("Index", lista);
+        }
+
+    }
+}
---
+++ inventario/Dato/InventarioAdmin.cs
@@ -0,0 +1,61 @@
+﻿using inventario.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace inventario.Dato
+{
+    public class InventarioAdmin
+    {
+        public IEnumerable<registros> Consultar()
+        {
+            using (crudEntities contexto = new crudEntities())
+            {
+                return contexto.registros.AsNoTracking().ToList();
+            }
+        }
+
+        //guarda un registro en la base de datos
+        public void Guardar(registros modelo)
+        {
+            using (crudEntities contexto = new crudEntities())
+            {
+                contexto.registros.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+
+        // detalles de los registros
+        public registros Consultar(int ID)
+        {
+            using (crudEntities contexto = new crudEntities())
+            {
+                return contexto.registros.FirstOrDefault(r => r.ID == ID);
+            }
+        }
+
+        //modificar los registros
+        public void Modificar(registros modelo)
+        {
+            using (crudEntities contexto = new crudEntities())
+            {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+
+        }
+
+        public void Eliminar(registros modelo)
+        {
+            using (crudEntities contexto = new crudEntities())
+            {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+
+        }
+
+
+    }
+}
